22a7661bb791522daa169863adcf558570749b49
Uliana Semyonova <i@ulianasemyonova.ru>
2023-08-11 17:10:10 +0500

    Create home and contacts functions

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        print(f'User {name} asks to contact her/him by email {email}')
+    return render(request, 'catalog/contacts.html')
