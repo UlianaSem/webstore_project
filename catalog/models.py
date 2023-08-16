d541f67fe685400c453620d0e6451b1ebfb0d272
Uliana Semyonova <i@ulianasemyonova.ru>
2023-08-16 21:42:32 +0500

    Create Category and Product classes

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    picture = models.ImageField(upload_to='catalog/', verbose_name='изображение', **NULLABLE)
+    category_id = models.ForeignKey(Category, on_delete=models.SET_NULL, **NULLABLE)
+    price = models.FloatField(verbose_name='цена', **NULLABLE)
+    create_date = models.DateField(verbose_name='дата создания', **NULLABLE)
+    change_date = models.DateField(verbose_name='дата изменения', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name} {self.price} в категории {self.category_id}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
